2fc6af9332d445d5d56e8eed336075e44e83ad15
anthonyykh <anthonykh01.ak@gmail.com>
2022-11-05 02:50:42 +0200

    added login page

---
+++ app/src/main/java/com/lau/socialmediaapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lau.socialmediaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void submit(View v){
+        username =findViewById(R.id.username_name);
+        username =findViewById(R.id.password);
+
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+        startActivity(intent);
+
+
+    }
+}
